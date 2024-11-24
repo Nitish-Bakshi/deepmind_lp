6d6b40c4b97d5ad363cc0507688471096f92559a
Nitish-Bakshi <nitishbakshi05@gmail.com>
2024-11-24 00:37:59 -0500

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ function Banner() {
   return (
     <div className="bannerDiv" id="banner">
       <img
-        src="https://images.unsplash.com/photo-1487537023671-8dce1a785863?q=80&w=2449&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="https://images.unsplash.com/photo-1453738773917-9c3eff1db985?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="Banner"
       />
       <h1>DEEPMIND AMA</h1>
